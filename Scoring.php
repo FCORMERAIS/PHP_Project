84e0fad34803fce103d71f8c1abd37ccc0379442
ARROUSSI Mehdi <mehdi.arroussi@ynov.com>
2022-11-15 15:41:23 +0100

    Verify -> get score

--- Scoring.php
+++ Scoring.php
@@ -5,17 +5,38 @@ session_start();
 //connexion à la base de donnée MySQL
 require("connectAccount.php");
 
+// On vérifie si l'utilisateur est connecté
+if(isset($_COOKIE["name"]) == true) {
+
+// On répurère les informations de l'utilisateur : son idGroup et sa date de dernière connexion
+$requete = $mysqli->query("SELECT idGroup, lastConnection FROM `user` WHERE Name = '".$_COOKIE["name"]."'");
+
+
+
+}
+else {
+  // Sinon on informe qu'il n'est pas connecté
+    echo "Vous n'êtes pas connecté";
+}
+
+
+
+
+
+
+
+
+
 //récupération des données du formulaire
-$login = $_POST['login'];
-$password = $_POST['password'];
 $idGroup = $_POST['idGroup'];
 $lastConnexion = $_POST['lastConnexion'];
 
 //On récupère le score du groupe dont fais partis l'utilisateur
-$sql = "SELECT score FROM `group` WHERE idGroup = '$idGroup'";
+$sql = "SELECT score FROM `groups` WHERE idGroup = '$idGroup'";
  $score = $_GET['score'];
 
-//On revoie le score de l'utilisateur sur Menu.php
-header("Location: /PHPProject/menu.php?score=$score");
+//On revoie le score de l'utilisateur à la page menu.php
+header("Location: menu.php?score=$score");
+
 
 ?>
