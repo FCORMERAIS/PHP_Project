0dcdceb5477965ee64649d6b8deb60077de24b34
Marius BOURSE <marius.bourse@ynov.com>
2022-11-20 19:10:53 +0100

    separete file for login and signin

--- src/component/Login.php
+++ src/component/Login.php
@@ -1,38 +1,6 @@
-<?php 
-  $returnmsg = "";
-  require("../func/connectAccount.php");
-    $mail = $_POST["email"];
-    $password = $_POST["password"];
-    if ($mail == NULL && $returnmsg == ""|| $password == NULL && $returnmsg == "") {
-      $returnmsg= "Please fill out the gaps ";
-    }
-    if ($returnmsg == "") {
-      if(testValueUserName($mail)=="" && $returnmsg == ""){
-        if (testValueUserMail($mail)=="" && $returnmsg == "") {
-            $returnmsg =  "sorry but the username or email is not good";
-        }else {
-          $nameUserCookie = testValueUserMail($mail);
-        }
-      }else {
-        $nameUserCookie = testValueUserName($mail);
-      }
-    }
-    if ($returnmsg == "") {
-      if(!testValuePassword($password,$nameUserCookie)){
-        $returnmsg =  "sorry but the password is not good";
-      }
-    }
-    
-    if($returnmsg == ""){
-      setcookie("name",$nameUserCookie,time()+36000,"/","localhost");
-      require("../func/checkHabit.php");
-      checkHabit($mail);
-      header("Location: /PHPProject/src/component/menu.php");
-      exit();
-    }
-?>
 
 <?php 
+include "../asset/phpLogin.php";
 include "../asset/headerLogin.php";
 ?>
 
