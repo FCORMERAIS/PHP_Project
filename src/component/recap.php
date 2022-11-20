b5fc85ec1131a6629c4a993990a527673077799f
Marius BOURSE <marius.bourse@ynov.com>
2022-11-20 20:09:39 +0100

    debut page recap

--- src/component/recap.php
+++ src/component/recap.php
@@ -5,5 +5,9 @@ include "../asset/header.php";
 ?>
     <body>
         
+        <form action="Menu.php" method="POST">
+            <input type="submit" value="COMPRIS !">
+
+        </form>
     </body>
 </html>
